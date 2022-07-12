cc5249f8364712cb8ec49ac75b44beefc9fc65cc
Maxim <25-nv-86@mail.ru>
2022-07-12 03:14:40 +0300

    6 домашняя работа, заметки

--- app/src/main/java/com/maximmesh/notes/DescriptionFragment.java
+++ app/src/main/java/com/maximmesh/notes/DescriptionFragment.java
@@ -1,15 +1,32 @@
 package com.maximmesh.notes;
 
+import android.app.DatePickerDialog;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class DescriptionFragment extends Fragment {
+
+   static final String ARG_INDEX = "index";
+
+
+
+
+
 
 
    public DescriptionFragment() {
@@ -22,6 +39,8 @@ public class DescriptionFragment extends Fragment {
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
+
+
    }
 
    @Override
@@ -30,4 +49,36 @@ public class DescriptionFragment extends Fragment {
       // Inflate the layout for this fragment
       return inflater.inflate(R.layout.fragment_description, container, false);
    }
+
+   @Override
+   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+      super.onViewCreated(view, savedInstanceState);
+      Bundle arguments = getArguments();
+
+
+      if(arguments != null){
+         int index = arguments.getInt(ARG_INDEX);
+
+         TextView textView = view.findViewById(R.id.fragment_description_text_view);
+         TypedArray description = getResources().obtainTypedArray(R.array.description_notes);
+         textView.setText(description.getResourceId(index, 0));
+         description.recycle();
+      }
+
+
+
+
+   }
+
+
+   public static DescriptionFragment newInstance(int index){
+      DescriptionFragment fragment = new DescriptionFragment();
+      Bundle args = new Bundle();
+      args.putInt(ARG_INDEX, index);
+      fragment.setArguments(args);
+      return fragment;
+
+
+   }
+
 }
--- app/src/main/java/com/maximmesh/notes/MainActivity.java
+++ app/src/main/java/com/maximmesh/notes/MainActivity.java
@@ -1,14 +1,41 @@
 package com.maximmesh.notes;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+      NotesFragment notesFragment = new NotesFragment();//сначала создаем фрагметн
+      getSupportFragmentManager()//обращаемся к фрагмент менеджеру (p.s. в рамках одной транзакции можно работать с несколькими фрагментами
+      .beginTransaction()
+      .replace(R.id.fragment_container, notesFragment) //удаляем предыдущий фрагмент, добавляем новый. если add, то поверх старого добавляем новый
+      .commit();
+
+      TextView resultDataPicker = findViewById(R.id.tv_result_data_picker);
+      DatePicker datePicker = findViewById(R.id.datePicker);
+
+      ImageButton imageButton = findViewById(R.id.add_button);
+      imageButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Добавляем новую заметку", Toast.LENGTH_LONG).show();
+         }
+      });
+
+
    }
 }
