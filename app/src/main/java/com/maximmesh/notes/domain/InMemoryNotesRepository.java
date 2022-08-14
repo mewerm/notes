2e7a4ccd435f8bc0867491c98348b516568f3078
Maxim <25-nv-86@mail.ru>
2022-08-14 21:13:38 +0300

    12 домашняя работа. Реализована задача сохранения данных заметок с помощью sharedPref GSON JSON

--- app/src/main/java/com/maximmesh/notes/domain/InMemoryNotesRepository.java
+++ app/src/main/java/com/maximmesh/notes/domain/InMemoryNotesRepository.java
@@ -19,13 +19,6 @@ public class InMemoryNotesRepository implements NotesRepository {
    private Handler handler = new Handler(Looper.getMainLooper()); //Handler - средство по доставке сообщений какому-либо потоку
 
    public InMemoryNotesRepository() {
-      data.add(new Note(UUID.randomUUID().toString(), "Здесь будут заголовки твоих заметок", "Здесь описание. Нажми плюсик внизу экрана =)", new Date()));
- /*       data.add(new Note(UUID.randomUUID().toString(), "Заметка 2", "Описаие заметки 2", new Date()));
-      data.add(new Note(UUID.randomUUID().toString(), "Заметка 3", "Описаие заметки 3", new Date()));
-      data.add(new Note(UUID.randomUUID().toString(), "Заметка 4", "Описаие заметки 4", new Date()));
-      data.add(new Note(UUID.randomUUID().toString(), "Заметка 5", "Описаие заметки 5", new Date()));
-      data.add(new Note(UUID.randomUUID().toString(), "Заметка 6", "Описаие заметки 6", new Date()));
-      data.add(new Note(UUID.randomUUID().toString(), "Заметка 7", "Описаие заметки 7", new Date()));*/
 
    }
 
--- app/src/main/java/com/maximmesh/notes/domain/SharedPrefNotesRepository.java
+++ app/src/main/java/com/maximmesh/notes/domain/SharedPrefNotesRepository.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.UUID;
 
 public class SharedPrefNotesRepository implements NotesRepository {
 
+   private static final String KEY_SAVED_NOTES = "KEY_SAVED_NOTES"; //ключ сохраненных заметок для sharPref
 
    //чтобы сохранить, нужно сначала заполучить из контекста:
    private Context context;
@@ -26,28 +29,44 @@ public class SharedPrefNotesRepository implements NotesRepository {
    @Override
    public void getAll(CallBack<List<Note>> callback) {
 
+      //получение списка заметок:
+
+      Gson gson = new Gson(); //создали объект gson
+
+      String savedData = sharedPreferences.getString(KEY_SAVED_NOTES, "[]"); //дефолтное значение "пустой список" для json это скобки "[]"
+
+      Type type = new TypeToken<ArrayList<Note>>() {}.getType(); //это конструкция, для получения типизированные по дженерику колекции. то есть мы не
+      //сможем передать в  gson.fromJson(savedData, List<Note>.class) - не можем передать напрямую List<Note>.class. только через эту обертку TypeToken
+
+      List<Note> savedNotes = gson.fromJson(savedData, type);//чтобы преобразовать строку в объекты нужна обертка та, что выше.
+
+      callback.onSuccess(savedNotes);
    }
 
    @Override
    public void addNote(String title, String message, CallBack<Note> callback) {
-      //сохранять будем сразу списком
-      ArrayList<Note> data = new ArrayList<>();
 
       Note note = new Note(UUID.randomUUID().toString(), title, message, new Date());
       //UUID.randomUUID - индификатор так добавляем. Также передали заголовок, тело и дату создания заметки)
 
       Gson gson = new Gson(); //создали объект gson
 
-      String json = gson.toJson(note); //передаем методу toJson тот объект, который хотим представить в виде строки
+      String savedData = sharedPreferences.getString(KEY_SAVED_NOTES, "[]"); //дефолтное значение "пустой список" для json это скобки "[]"
 
-      json.length();
+      Type type = new TypeToken<ArrayList<Note>>() {}.getType(); //это конструкция, для получения типизированные по дженерику колекции. то есть мы не
+      //сможем передать в  gson.fromJson(savedData, List<Note>.class) - не можем передать напрямую List<Note>.class. только через эту обертку TypeToken
 
-      /*
-      data.add(new Note(UUID.randomUUID().toString(), title, message, new Date())); //UUID.randomUUID - индификатор так добавляем. Также передали заголовок, тело и дату создания заметки
+      List<Note> savedNotes = gson.fromJson(savedData, type);//чтобы преобразовать строку в объекты нужна обертка та, что выше.
 
-      sharedPreferences.edit() //для сохранения, берем заметку
-      .putString()
-*/
+      savedNotes.add(note); //добавляем в список сохраненных заметок новую заметку
+
+      String toWrite = gson.toJson(savedNotes, type); //получили строку
+
+      sharedPreferences.edit()
+      .putString(KEY_SAVED_NOTES, toWrite) //положили стркоу
+      .apply();
+
+      callback.onSuccess(note);
 
    }
 
