ea37a33465caca2b369e719f2aaa9c18c7052d3b
Maxim <25-nv-86@mail.ru>
2022-08-14 23:36:22 +0300

    12 домашняя работа. Подключился к FireBase

--- app/src/main/java/com/maximmesh/notes/di/Dependencies.java
+++ app/src/main/java/com/maximmesh/notes/di/Dependencies.java
@@ -1,14 +1,10 @@
 package com.maximmesh.notes.di;
 
 import android.content.Context;
-
-import com.maximmesh.notes.domain.InMemoryNotesRepository;
 import com.maximmesh.notes.domain.NotesRepository;
 import com.maximmesh.notes.domain.SharedPrefNotesRepository;
 
 public class Dependencies { //класс зависимотси
-
-
 
    private static  NotesRepository notesRepository;
 
